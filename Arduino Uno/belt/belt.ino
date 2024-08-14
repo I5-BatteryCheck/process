af6c495148c06d6fcbb025924aa1424ca70a63dd
kingbeom-V <rlqja530@naver.com>
2024-08-14 21:08:23 +0900

    Update belt.ino

--- Arduino Uno/belt/belt.ino
+++ Arduino Uno/belt/belt.ino
@@ -1,11 +1,3 @@
-/******************************************************************************************
- * FileName      : belt.ino
- * Description   : An Arduino Uno board connected to a belt: When the first ultrasonic sensor detects an object, send a capture signal to the Raspberry Pi. 
-                  The belt is controlled based on the battery status determined by the second ultrasonic sensor
- * Author        : GiBeom Park
- * Last Modified : 2024.07.30
- ******************************************************************************************/
-//제품 초기 인식
 #define TRIG 10 //TRIG 핀 설정 (초음파 보내는 핀)
 #define ECHO 11 //ECHO 핀 설정 (초음파 받는 핀)
 
@@ -18,14 +10,15 @@
 
 #define CONVEYOR 3
 
+#define outPin 5 // 로봇팔과 연결된 디지털 핀 
 
-bool recived_data=false;
-bool defective=false;
+
+bool recived_data = false;
+bool defective = false;
+bool stopConveyor = false; // 컨베이어 벨트 멈춤 상태를 저장하는 변수
 
 void setup() {
   Serial.begin(9600); //PC모니터로 센서값을 확인하기위해서 시리얼 통신을 정의해줍니다. 
-                       //시리얼 통신을 이용해 PC모니터로 데이터 값을 확인하는 부분은 자주사용되기 때문에
-                       //필수로 습득해야하는 교육코스 입니다.
   Serial.setTimeout(10);
 
   pinMode(TRIG, OUTPUT);
@@ -38,43 +31,49 @@ void setup() {
   pinMode(SENSE_, OUTPUT);
 
   pinMode(CONVEYOR, OUTPUT);
-
+  pinMode(outPin, OUTPUT);
 }
 
 void loop() {
+  readSerial(); // 시리얼 데이터 먼저 읽기
+
   long distance = getDistance(TRIG, ECHO);
-  readSerial();
 
-  if (distance<10){ //첫번째 초음파 센서에 배터리 온 경우
-    digitalWrite(CONVEYOR, HIGH); //콘베이어벨트 멈춤
+  if (distance < 10) { // 첫번째 초음파 센서에 제품이 온 경우
+    digitalWrite(CONVEYOR, HIGH); // 콘베이어 벨트 멈춤
     digitalWrite(SENSE, HIGH);
-    Serial.print("CAPTURE"); //라즈베리파이에게 시리얼통신으로 보냄
-    delay(1000); //1초 대기
-    digitalWrite(CONVEYOR, LOW); //콘베이어벨트 작동
+    Serial.println("CAPTURE"); // 라즈베리파이에게 시리얼통신으로 보냄
+    delay(2000); // 1초 대기
+    digitalWrite(CONVEYOR, LOW); // 콘베이어 벨트 작동
+    delay(3000); // 1.5초 대기
   }
-
 
   long distance_ = getDistance(TRIG_, ECHO_);
 
-  if(distance_<10){ //첫번째 초음파 센서를 지나 두번째 초음파 센서에 온 경우
-    if(is_data_received()){ //시리얼 데이터를 받은 경우
-      if(is_defective()){ //불량품이면 멈춤
-       Serial.println("Data Received and stop for 5s");
-      digitalWrite(CONVEYOR, HIGH);//불량 제품일 때 , 멈춤
-      delay(5000); //5초 정지(로봇팔이 움직이는 충분한 속도로 재지정 필요)
-      digitalWrite(CONVEYOR, LOW);//콘베이어벨트 작동
-      }
-      else{
+  if (distance_ < 10) { // 두번째 초음파 센서에 제품이 온 경우
+    if (is_data_received()) { // 시리얼 데이터를 받은 경우
+      if (is_defective()) { // 불량품이면 멈춤
+        Serial.println("Data Received and stop for 5s and defect remove");
+
+        digitalWrite(CONVEYOR, HIGH); // 불량 제품일 때 벨트를 멈춤
+
+        digitalWrite(outPin, HIGH); // 로봇팔에 신호를 보냄
+        delay(1000); // 1초 대기
+        digitalWrite(outPin, LOW); // 신호를 끔
+
+        delay(5000); // 5초 정지
+        digitalWrite(CONVEYOR, LOW); // 콘베이어 벨트 작동
+        delay(2000);
+      } else { // 정상이면 지나감
         Serial.println("Data Received and pass");
-        digitalWrite(CONVEYOR, LOW);//콘베이어벨트 작동
+        digitalWrite(CONVEYOR, LOW); // 콘베이어 벨트 작동
+        delay(2000);
       }
-    }
-    else{//시리얼 데이터를 못 받은 경우
-      digitalWrite(CONVEYOR, HIGH);//컨베이어 밸트 대기
+    } else { // 시리얼 데이터를 못 받은 경우
+      digitalWrite(CONVEYOR, HIGH); // 콘베이어 벨트 대기
     }
   }
 }
-
 
 long getDistance(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
@@ -89,38 +88,36 @@ long getDistance(int trigPin, int echoPin) {
   return duration * 17 / 1000;
 }
 
-
-bool is_data_received(){
+bool is_data_received() {
   bool tmp = recived_data;
-  if(recived_data==true){
-    recived_data=false;
+  if (recived_data == true) {
+    recived_data = false;
   }
   return tmp;
 }
 
-bool is_defective(){
+bool is_defective() {
   bool tmp = defective;
-  if(defective==true){
-    defective=false;
+  if (defective == true) {
+    defective = false;
   }
   return tmp;
 }
 
-bool readSerial(){
+void readSerial() {
   if (Serial.available() > 0) {
-    // 시리얼 포트에서 한 줄 읽기
-    recived_data=true;
-    String incomingData = Serial.readStringUntil('\n'); //줄 바꿈 문자가 나올때 까지 읽음
-    if(incomingData=="True"){
-      defective=true;
-    }
-    if(incomingData=="False"){
-      defective=false;
+    String incomingData = Serial.readStringUntil('\n'); // 줄 바꿈 문자가 나올 때까지 읽음
+
+    if (incomingData == "Stop") {
+      stopConveyor = true; // 컨베이어 벨트 멈춤
+    } else if (incomingData == "Start") {
+      stopConveyor = false; // 컨베이어 벨트 동작
+    } else if (incomingData == "True") {
+      recived_data = true;
+      defective = false;
+    } else if (incomingData == "False") {
+      recived_data = true;
+      defective = true;
     }
   }
 }
-//=========================================================================================
-//
-// SW_Bootcamp I5
-//
-//=========================================================================================
