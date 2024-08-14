46d377aa9e8dce098231617bf71842c82dc587d2
GiBeom Park <rlqja530@gmail.com>
2024-08-14 14:29:19 +0900

    add vibration sensor and motor sensor

--- 아두이노/main/main.ino
+++ 아두이노/main/main.ino
@@ -1,43 +1,60 @@
 /******************************************************************************************
  * FileName     : main.ino
- * Description  : 온습도 센서, 조도 센서,가스 센서의 값을 HTTP를 통해 0.5초마다 전송
+ * Description  : 온습도 센서, 조도 센서, 가스 센서, 진동센서의 값을 HTTP를 통해 전송 / 온도 28도 초과면 모터(선풍기) 가동
  * Author       : 박기범
- * Created Date : 2024.07.24
+ * Created Date : 2024.08.14
  ******************************************************************************************/
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "dht11.h"
-#define DHT11PIN D2 // D2 포트에 DHT11 센서를 연결해야 함
+#include "wifi_credentials.h" // WiFi credentials 파일 포함
 
-const char* ssid = "Ccom";      // WiFi SSID
-const char* password = "ccom0414";  // WiFi 비밀번호
+#define DHT11PIN D3 // DHT11 센서를 연결
+#define electric_fan D2 //모터 센서 
+const char* postServerName = "http://192.168.137.515010/sensor"; //URL 수정
 dht11 DHT11; // 온습도 센서
-int light_sensor = A1; // 조도센서
-int gasSensorPin = A4; // MQ-2 센서의 아날로그 출력 핀
+int light_sensor = A1; // 조도 센서
+int gasSensorPin = A3; // MQ-2 센서
+int vibration_pin = A4; // 전동 센서
 
 struct SensorData {
   int temperature;
   int humidity;
   int lightLevel;
   int gas;
+  int vibrationArray[100]={0}; // 진동 배열을 포함하도록 수정
 };
+
+SensorData data; // 전역변수로 센서 데이터 저장
 
 void setup() {
   Serial.begin(9600);
+  pinMode(electric_fan,OUTPUT);
+  digitalWrite(electric_fan, LOW);
   setupWiFi();
 }
 
 void loop() {  
-  SensorData data = readSensors(); // 센서 값을 읽고 반환받음
+  unsigned long startTime = micros(); // loop 시작 시간 기록
+  unsigned long startTime22 = millis(); // loop 시작 시간 기록
+  for (int i = 0; i < 100; i++) {
+    data.vibrationArray[i] = analogRead(vibration_pin);
+    delayMicroseconds(40);  // 샘플링 레이트 조절
+  }
+  unsigned long endTime1 = micros(); // 종료 시간 측정
 
+  readSensors();
 
-  serialPrint(data); //시리얼 모니터에 출력 표시
+  if(data.temperature>28){
+    digitalWrite(electric_fan,HIGH);
+  }
+  else{
+    digitalWrite(electric_fan,LOW);
+  }
 
-  const char* postServerName = "http://192.168.137.51:5010/sensor"; //URL 수정
   String jsonPayload = createJsonPayload(data);
-  httpPostRequest(postServerName, jsonPayload);
   
-  delay(500);  // 0.5초 지연
+  httpPostRequest(postServerName, jsonPayload);
 }
 
 void serialPrint(SensorData data){
@@ -48,22 +65,19 @@ void serialPrint(SensorData data){
   Serial.print(data.humidity); // 습도 값 출력
   Serial.print("%, Light Level: ");
   Serial.print(data.lightLevel); // 조도 값 출력
-  Serial.println(" lux");
-  delay(100);
+  Serial.print(" lux");
   Serial.print("  gas: ");
-  Serial.println(analogRead(gasSensorPin)); // 가스 값 출력
+  Serial.println(data.gas); // 가스 값 출력
 }
 
-// DHT11 및 조도센서 값을 읽고 SensorData 구조체로 반환하는 함수
-SensorData readSensors() {
+//센서 값을 읽고 SensorData 구조체로 반환 
+void readSensors() {
   DHT11.read(DHT11PIN); // 온습도 센서(DTH11) 값 측정
   
-  SensorData data;
   data.temperature = DHT11.temperature;
   data.humidity = DHT11.humidity;
   data.lightLevel = analogRead(light_sensor); // 조도 값 측정
-  data.gas=analogRead(gasSensorPin);
-  return data;
+  data.gas = analogRead(gasSensorPin);
 }
 
 void setupWiFi() {
@@ -105,7 +119,17 @@ String createJsonPayload(SensorData data) {
   jsonPayload += "\"Temperature\":\"" + String(data.temperature) + "\",";
   jsonPayload += "\"humidity\":\"" + String(data.humidity) + "\",";
   jsonPayload += "\"lightLevel\":\"" + String(data.lightLevel) + "\",";
-  jsonPayload += "\"gas\":\"" + String(data.gas) + "\"";
+  jsonPayload += "\"gas\":\"" + String(data.gas) + "\",";
+  jsonPayload += "\"vibrationArray\":[";
+  
+  for (int i = 0; i < 100; i++) {
+    jsonPayload += String(data.vibrationArray[i]);
+    if (i < 99) {
+      jsonPayload += ",";
+    }
+  }
+  
+  jsonPayload += "]";
   jsonPayload += "}";
 
   Serial.println("JSON Payload: " + jsonPayload); // 디버그를 위해 JSON 출력
--- 아두이노/main/wifi_credentials.h
+++ 아두이노/main/wifi_credentials.h
@@ -2,7 +2,7 @@
 #ifndef WIFI_CREDENTIALS_H
 #define WIFI_CREDENTIALS_H
 
-const char* ssid = "Your_SSID";      // WiFi SSID
-const char* password = "Your_PASSWORD";  // WiFi 비밀번호
+const char* ssid = "Ccom";      // WiFi SSID
+const char* password = "ccom0414";  // WiFi 비밀번호
 
 #endif // WIFI_CREDENTIALS_H
