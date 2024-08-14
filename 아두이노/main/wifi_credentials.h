c459327b965d073ea707948b683e7d211cadb3ac
kingbeom-V <rlqja530@naver.com>
2024-08-14 14:31:34 +0900

    Delete 아두이노/main/wifi_credentials.h

--- 아두이노/main/wifi_credentials.h
+++
@@ -1,8 +0,0 @@
-// wifi_credentials.h
-#ifndef WIFI_CREDENTIALS_H
-#define WIFI_CREDENTIALS_H
-
-const char* ssid = "Ccom";      // WiFi SSID
-const char* password = "ccom0414";  // WiFi 비밀번호
-
-#endif // WIFI_CREDENTIALS_H
